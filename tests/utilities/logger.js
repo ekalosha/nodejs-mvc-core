17396d4bff17720536fb80d69d8468f4f3528e7c
sajera <allsajera@gmail.com>
2018-01-18 16:59:55 +0200

    * logger  -- module refactoring  -- tests

--- tests/utilities/logger.js
+++ tests/utilities/logger.js
@@ -4,7 +4,7 @@
 /**
  *
  */
-var logger = require('../../lib/utilities/logger');
+var Logger = require('../../lib/utilities/logger');
 
 /**
  *
@@ -16,38 +16,54 @@ var expect = require('chai').expect;
  */
 describe('utility LOGGER', function () {
 
-    it('shuld exist', function () {
-        expect(logger).to.be.a('object');
-    });
-
-    it('has class "Logger"', function () {
-        expect(logger.Logger).to.be.a('function');
+    it('class "Logger" should exist', function () {
+        expect(Logger).to.be.a('function');
     });
 
     it('has "instance"', function () {
-        expect(logger.instance).to.be.a('object');
+        expect(Logger.instance).to.be.a('object');
     });
 
     it('"instance" instanceof "Logger"', function () {
-        expect(logger.instance instanceof logger.Logger).to.be.true;
+        expect(Logger.instance instanceof Logger).to.be.true;
     });
 
     it('has method "replace"', function () {
-        expect(logger.replace).to.be.a('function');
+        expect(Logger.replace).to.be.a('function');
     });
 
     it('method "replace" => test', function () {
-        expect(logger.instance.testsProperties).to.not.be.true;
+        expect(Logger.instance.testsProperties, 'defaults NOT').to.not.be.true;
 
-        class CustomLogger extends logger.Logger {
+        class CustomLogger extends Logger {
             constructor () {
                 super();
                 this.testsProperties = true;
             }
         }
-        logger.replace(CustomLogger);
+        Logger.replace(CustomLogger);
+        expect(Logger.instance.testsProperties, 'custom YES').to.be.true;
+        expect(Logger.instance instanceof Logger, 'should stay instanceof "Logger"').to.be.true;
+    });
 
-        expect(logger.instance.testsProperties).to.be.true;
+    it('"instance" should has method "log"', function () {
+        expect(Logger.instance.log).to.be.a('function');
+    });
+
+    it('"instance" should has method "info"', function () {
+        expect(Logger.instance.info).to.be.a('function');
+    });
+
+    it('"instance" should has method "warn"', function () {
+        expect(Logger.instance.warn).to.be.a('function');
+    });
+
+    it('"instance" should has method "error"', function () {
+        expect(Logger.instance.error).to.be.a('function');
+    });
+
+    it('"instance" should has method "debug"', function () {
+        expect(Logger.instance.debug).to.be.a('function');
     });
 
 });
