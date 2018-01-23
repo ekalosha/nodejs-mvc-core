d63863c745e5c1d37676c4dcbfa4f9d8086f460f
sajera <allsajera@gmail.com>
2018-01-23 18:51:21 +0200

    *! new core  -- new dummy for entry point  -- test for basic logic

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,17 @@
 
 "use strict";
 
+
+/**
+ *
+ */
+var Core = require('../lib/core.js');
+
+/**
+ *
+ */
+var expect = require('chai').expect;
+
 /**
  * library TESTS
  */
@@ -8,6 +19,32 @@ describe('MVC Core', function () {
 
     require('./utilities/utilities.js');
 
+
+    it('should exist', function () {
+        expect(Core).to.be.a('function');
+    });
+
+    it('has static methods', function () {
+        expect(Core.on,  "on").to.be.a('function');
+        expect(Core.emit,  "emit").to.be.a('function');
+        // TODO connect others
+
+    });
+
+    it('has static props', function () {
+        expect(Core.instance,  "instance").to.be.a('object');
+        expect(Core.EVENT,  "EVENT").to.be.a('object');
+        expect(Core.logger,  "logger").to.be.a('object');
+        expect(Core.config,  "config").to.be.a('object');
+        // TODO connect others
+
+    });
+
+    it('has static Classes', function () {
+        expect(Core.Logger,  "Logger").to.be.a('function');
+        expect(Core.Configuration,  "Configuration").to.be.a('function');
+        // TODO connect others
+    });
 
 
 });
